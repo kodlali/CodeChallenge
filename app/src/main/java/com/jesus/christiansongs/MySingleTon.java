edb0626110864d1c137f1033f9e60f157f03954f
venkateswara_k <venkateswara_k@hcl.com>
2018-02-22 12:46:07 +0530

    intial commit

---
+++ app/src/main/java/com/jesus/christiansongs/MySingleTon.java
@@ -0,0 +1,21 @@
+package com.jesus.christiansongs;
+
+/**
+ * Created by venkateswara_k on 21-02-2018.
+ */
+
+public class MySingleTon {
+
+    private static MySingleTon instance;
+
+    private MySingleTon() {
+
+    }
+
+    public  static MySingleTon getIntstance(){
+
+
+        return null;
+    }
+
+}
